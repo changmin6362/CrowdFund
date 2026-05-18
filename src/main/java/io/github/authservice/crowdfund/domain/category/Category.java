5dfbd1fa28547bc58d5ce0dcce012085b3b3e5d9
KimChangmin <zzangmin3@gmail.com>
2026-05-18 22:11:39 +0900

    에러 수정

--- src/main/java/io/github/authservice/crowdfund/domain/category/Category.java
+++ src/main/java/io/github/authservice/crowdfund/domain/category/Category.java
@@ -14,6 +14,7 @@ import org.springframework.data.relational.core.mapping.Table;
  * @param isActive   활성 여부
  */
 @Table("category")
+@org.apache.ibatis.type.Alias("DomainCategory")
 public record Category(
     @Id Integer id,
     Integer parentId,
