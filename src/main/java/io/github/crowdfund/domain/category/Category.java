9c35a24fac5fbfa9b56660bdf335b9a5175de5fb
KimChangmin <zzangmin3@gmail.com>
2026-06-09 20:58:32 +0900

    fix: 카테고리 엔티티의 @Table 어노테이션 대소문자 수정

--- src/main/java/io/github/crowdfund/domain/category/Category.java
+++ src/main/java/io/github/crowdfund/domain/category/Category.java
@@ -6,7 +6,7 @@ import org.springframework.data.relational.core.mapping.Table;
 /**
  * 카테고리 테이블 매핑용 엔티티
  */
-@Table("Category")
+@Table("category")
 public record Category(
     @Id Integer id,
     Integer parentId,
